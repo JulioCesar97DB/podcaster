b6a323b74768a6e7cc982ed0bea6be316670b396
JCD <juliocesar97db@gmail.com>
2024-05-01 11:11:56 -0400

    fix: remove max length for card

--- src/components/PodcastCardList.js
+++ src/components/PodcastCardList.js
@@ -5,7 +5,7 @@ export const PodcastCardList = ({ id, summary, author, img, title }) => {
 
     return (
         <Link to={`/podcast/${id}`} state={{ summary, author, img, title }}>
-            <div className="w-60 max-h-44 bg-white shadow-lg border-2 border-gray-100 relative">
+            <div className="w-60 bg-white shadow-lg border-2 border-gray-100 relative">
                 <div className="absolute inset-x-0 top-0 h-5 flex items-center justify-center">
                     <img className="w-28 h-w-28 rounded-full border-4 border-white object-cover -mt-8" src={img} alt="Imagen del podcast" />
                 </div>
