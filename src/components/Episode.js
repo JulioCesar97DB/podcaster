2aa2be325d3d9d10a764c2d480de2b7833bab685
JCD <juliocesar97db@gmail.com>
2024-05-01 07:07:58 -0400

    feat: play the episode on a new page

---
+++ src/components/Episode.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import { PodcastCard } from './PodcastCard'
+import { useLocation } from 'react-router-dom'
+
+export const Episode = () => {
+    const {state:{summary, title, img, author, track}} = useLocation();
+    return (
+        <div className='flex'>
+            <div>
+                <PodcastCard img={img} author={author} title={title} summary={summary} />
+            </div>
+
+            <div className='flex flex-col p-4 h-min mt-10 mr-20 shadow-md shadow-slate-500'>
+                <div className='font-bold text-2xl'>{track.title}</div>
+                <div className='italic mb-2'>{track.description}</div>
+                {/* <div className='flex italic font-medium mb-4'>This episode is sponsored by <p>Sponsor, Sponsor2</p> and <p>Sponsor3</p>.</div> */}
+
+                <hr />
+
+                <audio className='border-none mt-4 min-w-full max-h-10' controls name='media'>
+                    <source type='audio/mpeg' src={track.episodeUrl} />
+                </audio>
+            </div>
+
+        </div>
+    )
+}
